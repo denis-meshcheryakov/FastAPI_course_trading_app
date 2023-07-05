80a7e68f8d44fb1ddccd7d40997c0a584c0135a8
denis-meshcheryakov <ktulhoo.pyneng@yandex.ru>
2023-07-05 17:02:48 +0300

    Изменил структуру проекта 2

--- Project/src/auth/manager.py
+++ Project/src/auth/manager.py
@@ -3,14 +3,15 @@ from typing import Optional
 from fastapi import Depends, Request
 from fastapi_users import BaseUserManager, IntegerIDMixin, exceptions, models, schemas
 
-from auth.database import User, get_user_db
+from auth.models import User
+from auth.utils import get_user_db
 
-SECRET = "SECRET"
+from config import SECRET_AUTH
 
 
 class UserManager(IntegerIDMixin, BaseUserManager[User, int]):
-    reset_password_token_secret = SECRET
-    verification_token_secret = SECRET
+    reset_password_token_secret = SECRET_AUTH
+    verification_token_secret = SECRET_AUTH
 
     async def on_after_register(self, user: User, request: Optional[Request] = None):
         print(f"User {user.id} has registered.")
@@ -21,7 +22,6 @@ class UserManager(IntegerIDMixin, BaseUserManager[User, int]):
         safe: bool = False,
         request: Optional[Request] = None,
     ) -> models.UP:
-        
         await self.validate_password(user_create.password, user_create)
 
         existing_user = await self.user_db.get_by_email(user_create.email)
@@ -43,6 +43,7 @@ class UserManager(IntegerIDMixin, BaseUserManager[User, int]):
 
         return created_user
 
-    
+
 async def get_user_manager(user_db=Depends(get_user_db)):
     yield UserManager(user_db)
+    
